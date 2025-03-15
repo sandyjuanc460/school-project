d2dc06e5de58456e6e0189d49d4d55dcd08a4da3
sandyjuanc460 <sandyjuanc460@gmail.com>
2025-03-15 14:23:45 +0000

    Add 9.c

---
+++ src/9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int age = 25;
+    char *name = "John";
+    char *address = "123 Main St.";
+
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Address: %s\n", address);
+
+    return 0;
+}
