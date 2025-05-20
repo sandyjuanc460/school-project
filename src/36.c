dd1ee064c5bd00615974b3d7b51906ef36997189
sandyjuanc460 <sandyjuanc460@gmail.com>
2025-05-20 05:32:22 +0000

    Add 36.c

---
+++ src/36.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    printf("Hello, World!\n");
+    for(i = 0; i < 5; i++) {
+        printf("* ");
+    }
+    printf("\n");
+    return 0;
+}
