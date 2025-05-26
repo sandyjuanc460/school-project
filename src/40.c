0b43019bbbc1fefe70801281e16c09e5fd12b838
sandyjuanc460 <sandyjuanc460@gmail.com>
2025-05-26 18:57:09 +0000

    Add 40.c

---
+++ src/40.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("Hello, World!\n");
+
+    for(i = 0; i < 5; i++) {
+        if(i % 2 == 0)
+            continue;
+        printf("*");
+    }
+
+    return 0;
+}
